08061f5b66a725cdd3d8438efc84c744d53102b1
jules onguene messolo <julesmessolo@gmail.com>
2022-11-01 00:59:09 +0100

    test vers heroku modification du front

--- FrondEndAngular/staffBoxe/src/environments/environment.prod.ts
+++ FrondEndAngular/staffBoxe/src/environments/environment.prod.ts
@@ -3,6 +3,11 @@ export const environment = {
 
   UrlAdherent: "https://managerstaffboxe.herokuapp.com/api/adherent",
 
-  UrlSection: "https://managerstaffboxe.herokuapp.com/api/section/all"  
+  UrlSection: "https://managerstaffboxe.herokuapp.com/api/section/all",
+
+  UrlSexe: "https://managerstaffboxe.herokuapp.com/api/sexe",
+
+  UrlCategorie: "https://managerstaffboxe.herokuapp.com/api/categorie"
+   
  
 };
